039f75a743ce9d443ced711e0b9fcfeb4818cc6b
Nicolas Raffonaud <nicolasraffonaudsoft@gmail.com>
2024-07-13 15:10:52 -0300

    Implementaciones desde casa

--- Ecommerce/src/App.jsx
+++ Ecommerce/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react"
 import NavBar from "./components/NavBar/NavBar"
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
-
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
 function App() {
     const [cartCount, setCartCount] = useState(5)
 
@@ -9,11 +10,19 @@ function App() {
       setCartCount ( x => x+1 )
     }
     return (
-    <>
+    <BrowserRouter>
       <NavBar cartCount={cartCount} title="Tienda de componentes de PC" />
       <button className="btn btn-success d-flex m-auto mt-5" onClick={handleCount}>Sumar carrito</button>
-      <ItemListContainer greetings="Bienvenido a la Guarida del HardWare" />
-    </>
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/category/:id" element={<ItemListContainer />} />
+        <Route path="/item/:id" element={<ItemDetailContainer />} />
+          
+          <ItemListContainer greetings="Bienvenido a la Guarida del HardWare" />
+          <ItemListContainer />
+          <ItemDetailContainer />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
