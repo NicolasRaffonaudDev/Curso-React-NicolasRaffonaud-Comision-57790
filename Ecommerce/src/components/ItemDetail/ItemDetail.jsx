c7b6c06ef5cf4d03418d53d0725f8d04fe488a85
Nicolas Raffonaud <nicolasraffonaudsoft@gmail.com>
2024-08-07 22:42:07 -0300

    corrigiendo detalles, terminando con material de la clase 10 de eventos

--- Ecommerce/src/components/ItemDetail/ItemDetail.jsx
+++ Ecommerce/src/components/ItemDetail/ItemDetail.jsx
@@ -3,12 +3,12 @@ import "./ItemDetail.css"
 
 const ItemDetail = ({name, img, price, category, description, stock}) => {
   return (
-    <article className="tarjetaProducto">
+    <article className="tarjetaProducto p-3 fs-4">
         <h1>{name}</h1>
-        <img src={img} style={{width:300}}></img>
+        <img className="m-5" src={img} style={{width:300}}></img>
         <p>Category: {category}</p>
         <p>{description}</p>
-        <h2>{price}</h2>
+        <h2 className="fs-1 text-danger">${price}</h2>
         <ItemCount stock={stock} />
     </article>
   )
