94adbbe9b229e0e62d37de84c3aa9db2eadcbcff
Nicolas Raffonaud <nicolasraffonaudsoft@gmail.com>
2024-08-09 12:49:56 -0300

    probando material clase 11, dark mode, faltan estilos, minuto 50

--- Ecommerce/src/App.jsx
+++ Ecommerce/src/App.jsx
@@ -1,30 +1,34 @@
-import { useState } from "react"
-import NavBar from "./components/NavBar/NavBar"
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
-import { createContext } from "react"
+import { useContext } from "react";
+import { ThemeContext, ThemeProvider } from "./context/ThemeContext";
 
-export const Contexto = createContext()
 
 function App() {
-    const [cartCount, setCartCount] = useState(0)
+  return (
+    <ThemeProvider>
+      <Home/>
+    </ThemeProvider>
+  );
+}
 
-    const handleCount = () => {
-      setCartCount ( x => x+1 )
-    }
-    return (
-    <BrowserRouter>
-      <Contexto.Provider value={10}>
-        <NavBar cartCount={cartCount} title="HardWare Raffonaud" />
-        <button className="btn btn-success d-flex m-auto mt-5" onClick={handleCount}>Sumar carrito</button>
-        <Routes>
-          <Route path="/" element={<ItemListContainer greetings="Bienvenido a la Guarida del HardWare."/>} />
-          <Route path="/category/:category" element={<ItemListContainer />} />
-          <Route path="/detail/:id" element={<ItemDetailContainer />} />
-        </Routes>
-      </Contexto.Provider>
-    </BrowserRouter>
+function Home() {
+  
+  const { isDarkMode } = useContext(ThemeContext)
+  
+  return (
+    <div className={isDarkMode ? "dark-mode" : "light-mode"}>
+      <h1>{isDarkMode ? "Dark Mode" : "Light Mode"}</h1>
+      <ThemeToggle />
+    </div>
+  )
+}
+
+const ThemeToggle = () => {
+  const {isDarkMode, toggleTheme} = useContext(ThemeContext)
+
+  return (
+    <button onClick={toggleTheme}>
+      {isDarkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+    </button>
   )
 }
 
